07b90af721048d1e85cee5701addf9cdc0b8ec84
sabaelhilo <saba.elhilo@mapbox.com>
2019-06-12 11:37:26 -0700

    comment out reqs

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ with open('trace_classifier/__init__.py') as f:
 
 # Requirements
 requirements = [
-    'keras==2.2.2',
-    'tensorflow==1.10.0',
-    'numpy==1.14.5',
-    'pandas==0.23.1',
-    'matplotlib==3.0.0',
-    'h5py==2.8.0',
-    'pyspark==2.3.1',
-    'pyarrow==0.8.0'       # required by pyspark
+    # 'keras==2.2.2',
+    # 'tensorflow==1.10.0',
+    # 'numpy==1.14.5',
+    # 'pandas==0.23.1',
+    # 'matplotlib==3.0.0',
+    # 'h5py==2.8.0',
+    # 'pyspark==2.3.1',
+    # 'pyarrow==0.8.0'       # required by pyspark
 ]
 
 # Extra requirements
